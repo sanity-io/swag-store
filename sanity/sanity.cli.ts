775a576c7733187635fbfc9a66ead185a85ac0d2
Kevin Green <kevin.green@sanity.io>
2025-10-17 13:39:55 -0700

    chore: more tuning for github workflow deployment

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -7,7 +7,7 @@
 
 import {defineCliConfig} from 'sanity/cli'
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '<your project ID>'
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'l3u4li5b'
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
 
 export default defineCliConfig({
@@ -16,12 +16,12 @@ export default defineCliConfig({
     dataset
   },
   deployment: {
-    appId: 'm2123kpoguhqkpk659m00j7n'
+    appId: 'm2123kpoguhqkpk659m00j7n',
+    autoUpdates: true,
   },
   // studioHost: process.env.SANITY_STUDIO_HOST || 'localhost',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
-  autoUpdates: true,
 })
